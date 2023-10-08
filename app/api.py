a6cf8da3c04a265521192c7ecbc9ecbcdc14dfc9
dima <d.ivanov@cism-ms.ru>
2023-10-08 22:10:07 +0300

    helm

--- app/api.py
+++ app/api.py
@@ -1,85 +1,34 @@
-import time
 import random
+import time
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter
 
-from db import SessionLocal
-import crud, schemas
-
-user_router = APIRouter()
 app_router = APIRouter()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@user_router.post("/", summary="Create user")
-def create_user(user_dto: schemas.CreateUserDto, db: Session = Depends(get_db)):
-    """
-    Создание пользователя:
-
-    - **username**: логин
-    - **firstName**: имя
-    - **lastName**: фамилия
-    - **email**: email
-    - **phone**: Телефон
-    """
+@app_router.get("/method-1", summary="method-1")
+def method_1():
     time.sleep(random.randint(1, 10))
     if random.choice([True, False]):
         raise Exception("test exception")
-    return crud.create_user(db, user_dto)
+    return True
 
 
-@user_router.get("/{userId}/", summary="ID of user")
-def get_user(userId: int, db: Session = Depends(get_db)):
-    """
-    Информация о пользователе по id:
-
-    - **id**: идентификатор пользователя
-    """
-    time.sleep(random.randint(1, 10))
+@app_router.post("/method-2", summary="method-2")
+def method_2():
+    time.sleep(random.randint(1, 2))
     if random.choice([True, False]):
         raise Exception("test exception")
-    return crud.get_user_by_id(db, userId)
+    return True
 
 
-@user_router.delete("/{userId}/", summary="delete user")
-def delete_user(userId: int, db: Session = Depends(get_db)):
-    """
-    Удаление пользователя по id:
-
-    - **id**: идентификатор пользователя
-    """
-    time.sleep(random.randint(1, 10))
+@app_router.delete("/method-3", summary="method-3")
+def method_3():
+    time.sleep(random.randint(3, 5))
     if random.choice([True, False]):
         raise Exception("test exception")
-    return crud.delete_user_by_id(db, userId)
-
-
-@user_router.put("/{userId}/", summary="update user")
-def update_user(userId: int, user_dto: schemas.UpdateUserDto, db: Session = Depends(get_db)):
-    """
-    Обновление пользователя по id:
-
-    - **id**: идентификатор пользователя
-    - **firstName**: имя
-    - **lastName**: фамилия
-    - **email**: email
-    - **phone**: Телефон
-    """
-    time.sleep(random.randint(1, 10))
-    if random.choice([True, False]):
-        raise Exception("test exception")
-    return crud.update_user_by_id(db, userId, user_dto)
+    return True
 
 
 api_router = APIRouter()
-
-api_router.include_router(user_router, prefix="/user", tags=["user"])
 api_router.include_router(app_router, prefix="/app", tags=["app"])
--- app/app_config.py
+++ app/app_config.py
@@ -2,9 +2,8 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "test3"
+    PROJECT_NAME: str = "otus-4"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str = Field(..., env="DATABASE_URL")
 
     class Config:
         env_file = ".env"
